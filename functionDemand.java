30d6224506567620a40bdfbb2bed8b4d4c432548
MurzikProject <antoshkae985@gmail.com>
2016-03-16 17:14:10 +0200

    version 1.01

--- functionDemand.java
+++ functionDemand.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class functionDemand implements Function {
 
 	private double k;
 	private double b;
 
-	public functionDemand(double k, double b) {
-		this.k=k;
-		this.b=b;
+	public functionDemand() {
+		String text4 = "Ok, lets start with demand function. You need to enter 2 coefficients: "+
+							"k and b."+
+							" Remember, the coefficient k in demand function always use with minus.";
+		String text5 = "Please, enter the coefficient b (not negative number).";
+		double temp_k;
+		double temp_b;
+		
+		System.out.println(text4);
+
+		Scanner dk = new Scanner(System.in);
+
+		if (dk.hasNextDouble()) {
+			temp_k = dk.nextDouble();
+			if (temp_k<0) {
+				this.k=temp_k;
+			} else {
+				System.out.println("The coefficient k in demand function always use with minus!!!");
+			}
+		} else {
+			System.out.println("You entered not correct data type!");
+		}
+
+		System.out.println(text5);
+
+		Scanner db = new Scanner(System.in);
+
+		if (db.hasNextDouble()) {
+			temp_b=db.nextDouble();
+			if (temp_b>=0) {
+				this.b=temp_b;
+			} else {
+				System.out.println("The coefficient b in demand function always isn't negative!!!");
+			}
+		} else {
+			System.out.println("You entered not correct data type!");
+		}
 	}
 
 	public void printFunction() {
--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 public class main {
+	
 	public static void main(String[] args) {
-		System.out.println("You should enter the coefficients of demand and supply functions.");
-		System.out.println("Notice. We use the linear function like this: y=k*x+b.");
-		System.out.println("Where y means Q - quantity, and x means P - price.");
-		System.out.println("Ok, lets start with demand function. You need to enter 2 coefficients: "+
-							"k and b."+
-							"Remember, the coeficient k in demand function always use with minus.");
+		String text1 = "You should enter the coefficients of demand and supply functions.";
+		String text2 = "Notice. We use the linear function like this: y=k*x+b.";
+		String text3 = "Where y means Q - quantity, and x means P - price.";
 
-		functionDemand demand1 = new functionDemand(-1.0, 8);
+		System.out.println(text1);
+		System.out.println(text2);
+		System.out.println(text3);
+
+		functionDemand demand1 = new functionDemand();
 		functionSupply supply1 = new functionSupply(2.0, -10.0);
 
 		demand1.printFunction();
